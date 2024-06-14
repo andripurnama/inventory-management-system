22fafae9e7b76119ce3d17aad212805721a15e83
andripurnama <andri.purnama995@hotmail.com>
2024-06-14 13:30:37 +0700

    feat: added order form

--- app/Filament/Resources/OrderResource.php
+++ app/Filament/Resources/OrderResource.php
@@ -3,28 +3,84 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\OrderResource\Pages;
-use App\Filament\Resources\OrderResource\RelationManagers;
+use App\Models\Customer;
+use App\Models\Item;
 use App\Models\Order;
 use Filament\Forms;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Facades\Log;
 
 class OrderResource extends Resource
 {
     protected static ?string $model = Order::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-m-shopping-cart';
 
     protected static ?string $navigationGroup = 'Transaction';
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                //
+                Select::make('customer_id')
+                    ->label('Customer Name')
+                    ->options(Customer::all()->pluck('name', 'id'))
+                    ->searchable(),
+                Select::make('status')->label('Status')->options(['Unpaid', 'Paid']),
+                Repeater::make('order_items')
+                    ->relationship('orderItems')
+                    ->schema([
+                        Forms\Components\Select::make('item_id')
+                            ->relationship('item', 'name')
+                            ->required()
+                            ->reactive()
+                            ->afterStateUpdated(function (callable $set, callable $get, $state) {
+                                $item = Item::find($state);
+                                if ($item) {
+                                    $set('price', $item->sale_price);
+                                    $quantity = $get('quantity') ?: 0;
+                                    $set('total', $quantity * $item->sale_price);
+                                }
+                                // static::recalculateGrandTotal($set, $get);
+                            }),
+                        Forms\Components\TextInput::make('quantity')
+                            ->required()
+                            ->integer()
+                            ->reactive()
+                            ->afterStateUpdated(function (callable $set, callable $get) {
+                                $quantity = $get('quantity');
+                                $price = $get('price');
+                                $total = $quantity * $price;
+                                $set('total', $total);
+                                static::recalculateGrandTotal($set, $get);
+                            }),
+                        Forms\Components\TextInput::make('price')
+                            ->required()
+                            ->numeric()
+                            ->disabled()
+                            ->step(0.01),
+                        Forms\Components\TextInput::make('total')
+                            ->disabled()
+                            ->numeric()
+                            ->step(0.01)
+                            ->reactive()
+                    ])
+                    ->columnSpanFull()
+                    ->columns(3)
+                    ->addActionLabel('Add Order Item')
+                    ->afterStateUpdated(function (callable $set, callable $get) {
+                        // static::recalculateGrandTotal($set, $get);
+                    }),
+                TextInput::make('grand_total')->disabled()->numeric(),
+
             ]);
     }
 
@@ -32,7 +88,9 @@ class OrderResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('order_reference')->searchable()->sortable(),
+                TextColumn::make('customer.name')->searchable()->sortable(),
+                TextColumn::make('status')->badge(),
             ])
             ->filters([
                 //
@@ -61,5 +119,24 @@ class OrderResource extends Resource
             'create' => Pages\CreateOrder::route('/create'),
             'edit' => Pages\EditOrder::route('/{record}/edit'),
         ];
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()->withoutGlobalScope(SoftDeletingScope::class);
+    }
+
+    public static function getGloballySearchableAttributes(): array
+    {
+        return ['number', 'customer.name'];
+    }
+
+    protected static function recalculateGrandTotal(callable $set, callable $get)
+    {
+        $orderItems = $get('order_items') ?? [];
+        $grandTotal = array_reduce($orderItems, function ($total, $item) {
+            return $total + ($item['quantity'] * $item['price']);
+        }, 0);
+        $set('grand_total', $grandTotal);
     }
 }
