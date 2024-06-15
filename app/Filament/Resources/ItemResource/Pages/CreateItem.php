642a8d6c52b80c8c765f57e83e0ec0128b5b6579
andripurnama <andri.purnama995@hotmail.com>
2024-06-15 14:35:24 +0700

    feat: added redirect url after save and success notification text

--- app/Filament/Resources/ItemResource/Pages/CreateItem.php
+++ app/Filament/Resources/ItemResource/Pages/CreateItem.php
@@ -9,4 +9,15 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateItem extends CreateRecord
 {
     protected static string $resource = ItemResource::class;
+    protected static bool $canCreateAnother = false;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function getCreatedNotificationTitle(): ?string
+    {
+        return 'Item has been created successfully';
+    }
 }
