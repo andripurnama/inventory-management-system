ab3004e7ca3692942c90c722bf9341360875cfbf
andripurnama <andri.purnama995@hotmail.com>
2024-06-15 14:27:06 +0700

    feat: added soft delete and model relation with JournalDetail

--- app/Models/Journal.php
+++ app/Models/Journal.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Journal extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    public function journalDetails(): HasMany
+    {
+        return $this->hasMany(JournalDetail::class);
+    }
 }
