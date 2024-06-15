96e1c740a5ed7dc01d53a23cf767a6c08cdbc2eb
andripurnama <andri.purnama995@hotmail.com>
2024-06-15 14:28:00 +0700

    feat: added model relation

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class OrderItem extends Model
 {
     use HasFactory;
+
+    public function item(): BelongsTo
+    {
+        return $this->belongsTo(Item::class);
+    }
 }
