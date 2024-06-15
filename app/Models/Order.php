0aaa254f9fdeac281c3077b48b1c202041d89e96
andripurnama <andri.purnama995@hotmail.com>
2024-06-15 14:27:43 +0700

    feat: added function to generate reference number and model relations

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use sirajcse\UniqueIdGenerator\UniqueIdGenerator;
 
 class Order extends Model
 {
     use HasFactory;
+
+    public function customer(): BelongsTo
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function orderItems(): HasMany
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+        self::creating(function ($model) {
+            $model->order_reference = UniqueIdGenerator::generate(['table' => $model->getTable(), 'length' => 10, 'prefix' => 'ORD-', 'suffix' => date('-dmyhis'), 'field' => 'order_reference']);
+        });
+    }
 }
