cae1a5e9edc818fc91cc12d90eec2a780e578fd1
andripurnama <andri.purnama995@hotmail.com>
2024-06-15 14:28:33 +0700

    feat: added generate reference number function and model relation

--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use sirajcse\UniqueIdGenerator\UniqueIdGenerator;
 
 class Supplier extends Model
 {
     use HasFactory;
+
+    public static function boot()
+    {
+        parent::boot();
+        self::creating(function ($model) {
+            $model->supplier_reference = UniqueIdGenerator::generate(['table' => $model->getTable(), 'length' => 10, 'prefix' => 'SUP-', 'suffix' => date('-dmyhis'), 'field' => 'supplier_reference']);
+        });
+    }
+    public function purchases(): HasMany
+    {
+        return $this->hasMany(Purchase::class);
+    }
 }
