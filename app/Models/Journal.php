0d5311b157a62f71ba78c507393c3951a0f431a2
andripurnama <andri.purnama995@hotmail.com>
2024-06-16 20:25:16 +0700

    feat: added generate reference number function

--- app/Models/Journal.php
+++ app/Models/Journal.php
@@ -6,13 +6,22 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use sirajcse\UniqueIdGenerator\UniqueIdGenerator;
 
 class Journal extends Model
 {
     use HasFactory, SoftDeletes;
-
+    protected $fillable = ['reference', 'remarks', 'date'];
     public function journalDetails(): HasMany
     {
         return $this->hasMany(JournalDetail::class);
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+        self::creating(function ($model) {
+            $model->reference = UniqueIdGenerator::generate(['table' => $model->getTable(), 'length' => 10, 'prefix' => 'GR-', 'suffix' => date('-dmyhis'), 'field' => 'order_reference']);
+        });
     }
 }
