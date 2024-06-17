8057fe827ea035addb0d65577e09bf129940c470
andripurnama <andri.purnama995@hotmail.com>
2024-06-17 21:07:11 +0700

    feat: added enable database transaction, form input and table columns

--- app/Filament/Resources/JournalResource.php
+++ app/Filament/Resources/JournalResource.php
@@ -4,7 +4,10 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\JournalResource\Pages;
 use App\Filament\Resources\JournalResource\RelationManagers;
+use App\Models\Account;
+use App\Models\Item;
 use App\Models\Journal;
+use Filament\Actions\DeleteAction;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
@@ -12,6 +15,12 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Tables\Columns\TextColumn;
+use Icetalker\FilamentTableRepeater\Forms\Components\TableRepeater;
 
 class JournalResource extends Resource
 {
@@ -20,11 +29,39 @@ class JournalResource extends Resource
     protected static ?string $navigationIcon = 'heroicon-c-document-check';
 
     protected static ?string $navigationGroup = 'Finance';
+
+    protected ?bool $hasDatabaseTransactions = true;
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                //
+                DatePicker::make('date')
+                    ->native(false)
+                    ->displayFormat('d F Y')
+                    ->locale('id'),
+                Textarea::make('remarks')->required(),
+                TableRepeater::make('journalDetails')
+                    ->relationship()
+                    ->label('Journal Detail')
+                    ->schema([
+                        Forms\Components\Select::make('account_id')
+                            ->relationship('account', 'name')
+                            ->label('Akun')
+                            ->options(Account::all()->pluck('name', 'id'))
+                            ->required()
+                            ->searchable(),
+                        TextInput::make('debit')
+                            ->required()
+                            ->numeric()
+                            ->inputMode('decimal'),
+                        TextInput::make('credit')
+                            ->required()
+                            ->numeric()
+                            ->inputMode('decimal'),
+
+                    ])
+                    ->collapsible(false)
+                    ->defaultItems(2)
             ]);
     }
 
@@ -32,13 +69,17 @@ class JournalResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('reference')->searchable()->sortable(),
+                TextColumn::make('date')->searchable()->sortable(),
+                TextColumn::make('remark')
             ])
             ->filters([
                 Tables\Filters\TrashedFilter::make(),
             ])
             ->actions([
+                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
