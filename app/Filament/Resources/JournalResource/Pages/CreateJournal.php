030ee003d5ad9a88e4cb8b6c4c74a2302925cdcf
andripurnama <andri.purnama995@hotmail.com>
2024-06-17 21:47:11 +0700

    feat: disabled create another one, add redirect url and customize success notif text

--- app/Filament/Resources/JournalResource/Pages/CreateJournal.php
+++ app/Filament/Resources/JournalResource/Pages/CreateJournal.php
@@ -9,4 +9,15 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateJournal extends CreateRecord
 {
     protected static string $resource = JournalResource::class;
+    protected static bool $canCreateAnother = false;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function getCreatedNotificationTitle(): ?string
+    {
+        return 'Journal has been created successfully';
+    }
 }
