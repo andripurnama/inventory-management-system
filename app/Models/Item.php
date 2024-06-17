81eca43b30c08d3f19f72fc6900563d4c6a40574
andripurnama <andri.purnama995@hotmail.com>
2024-06-17 10:33:15 +0700

    feat: added model relations with purchase and order

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -4,9 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Item extends Model
 {
     use HasFactory, SoftDeletes;
+
+    public function purchases(): HasMany
+    {
+        return $this->hasMany(PurchaseOrderDetail::class);
+    }
+
+    public function orders(): HasMany
+    {
+        return $this->hasMany(OrderItem::class);
+    }
 }
