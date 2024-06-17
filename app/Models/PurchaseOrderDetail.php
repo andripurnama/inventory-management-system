befcd1e4f934b569e73846e9839586f9c6de1c24
andripurnama <andri.purnama995@hotmail.com>
2024-06-17 10:39:11 +0700

    feat: added model relations

--- app/Models/PurchaseOrderDetail.php
+++ app/Models/PurchaseOrderDetail.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class PurchaseOrderDetail extends Model
 {
     use HasFactory;
+
+    public function item(): BelongsTo
+    {
+        return $this->belongsTo(Item::class);
+    }
+
+    public function purchase(): BelongsTo
+    {
+        return $this->belongsTo(Purchase::class, 'purchase_id', 'id');
+    }
 }
