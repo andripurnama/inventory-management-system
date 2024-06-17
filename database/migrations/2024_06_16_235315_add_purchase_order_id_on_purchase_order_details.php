27ffaa5277f695f879367bbbbc6a2b01cb67f7d8
andripurnama <andri.purnama995@hotmail.com>
2024-06-17 21:47:47 +0700

    feat: added purchase_id column on purchase order details

---
+++ database/migrations/2024_06_16_235315_add_purchase_order_id_on_purchase_order_details.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('purchase_order_details', function (Blueprint $table) {
+            $table->unsignedBigInteger('purchase_id')->after('id');
+            $table->foreign('purchase_order_id')->references('id')->on('purchase_orders')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('purchase_order_details', function (Blueprint $table) {
+            //
+        });
+    }
+};
