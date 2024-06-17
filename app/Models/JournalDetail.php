519433f592c2311695cb5d4b8a3fe020a63d7195
andripurnama <andri.purnama995@hotmail.com>
2024-06-17 10:26:35 +0700

    feat: added relation with journal and fillable columns

--- app/Models/JournalDetail.php
+++ app/Models/JournalDetail.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class JournalDetail extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['journal_id', 'account_id', 'reference', 'reference_type', 'debit', 'credit'];
+
+    public function journal(): BelongsTo
+    {
+        return $this->belongsTo(Journal::class);
+    }
 }
