f53a2f5141da7700e6d6b34f6b28499ed92d66fe
andripurnama <andri.purnama995@hotmail.com>
2024-06-17 10:32:54 +0700

    feat: added DB transaction for entry journal

--- app/Filament/Resources/OrderResource/Pages/CreateOrder.php
+++ app/Filament/Resources/OrderResource/Pages/CreateOrder.php
@@ -6,6 +6,7 @@ use App\Filament\Resources\OrderResource;
 use App\Models\Account;
 use App\Models\Item;
 use App\Models\Journal;
+use App\Models\JournalDetail;
 use App\Models\Order;
 use Carbon\Carbon;
 use Exception;
@@ -29,52 +30,31 @@ class CreateOrder extends CreateRecord
         return 'Order has been created successfully';
     }
 
-    protected function mutateFormDataBeforeCreate(array $data): array
+    protected function afterCreate(): void
     {
-        Log::debug('form data', $data);
-        DB::transaction(function () use ($data) {
-            $order = Order::create([
-                'grand_total' => $data['grand_total'],
-                'customer_id' => $data['customer_id'],
-                'status' => $data['status']
+        $accountId = Account::where('code', '1162.00.00')->first()->code;
+        // Runs after the form fields are saved to the database.
+        DB::transaction(function () use ($accountId) {
+            $journal = Journal::create([
+                'remarks' => 'Sales Order Number : ' . $this->record->order_reference,
+                'date' => Carbon::now()
             ]);
-
-            foreach ($data['order_items'] as $orderItem) {
-                $item = Item::find($orderItem['item_id']);
-
-                // Decrement the stock
-                if ($item->stock >= $orderItem['quantity']) {
-                    $item->stock -= $orderItem['quantity'];
-                    $item->save();
-
-                    $order->orderItems()->create([
-                        'item_id' => $orderItem['item_id'],
-                        'quantity' => $orderItem['quantity'],
-                        'price' => $orderItem['price'],
-                    ]);
-
-                    $accountDebetId = Account::where('name', 'Persediaan Barang Jadi')->first()->id;
-                    $journal = Journal::create(['remarks' => $order->order_reference, 'date' => Carbon::now()]);
-                    $journal->journalDetails()->create([
-                        'account_id' => $accountDebetId,
-                        'reference' => $order->order_reference,
-                        'reference_type' => 'order',
-                        'debit' => $orderItem['price'],
-                        'remarks' => 'Persediaan Barang Jadi' . $item->name . ' dengan order no ' . $order->order_reference
-                    ]);
-                    $accountCreditId = Account::where('name', 'Hutang Belum Ditagihkan')->first()->id;
-                    $journal->journalDetails()->create([
-                        'account_id' => $accountCreditId,
-                        'reference' => $order->order_reference,
-                        'reference_type' => 'order',
-                        'credit' => $orderItem['price'],
-                        'remarks' => 'Hutang Belum ditagihkan untuk' . $item->name . ' dengan order no ' . $order->order_reference
-                    ]);
-                } else {
-                    throw new Exception("Not enough stock for item: " . $item->name);
-                }
-            }
+            $journalDetail = JournalDetail::create([
+                'journal_id' => $journal->id,
+                'account_id' => $accountId,
+                'reference' => $this->record->order_reference,
+                'reference_type' => 'Sales Order',
+                'debit' => 0,
+                'credit' => $this->record->grand_total
+            ]);
+            JournalDetail::create([
+                'journal_id' => $journal->id,
+                'account_id' => $accountId,
+                'reference' => $this->record->order_reference,
+                'reference_type' => 'Sales Order',
+                'debit' => 0,
+                'credit' => $this->record->grand_total
+            ]);
         });
-        return $data;
     }
 }
